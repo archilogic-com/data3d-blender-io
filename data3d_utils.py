c217d80a6f219c05554563356a6c57b2acbd0025
Madlaina Kalunder <kalunder@archilogic.com>
2016-07-11 17:52:59 +0200

    Refactoring

--- data3d_utils.py
+++ data3d_utils.py
@@ -2,11 +2,15 @@ import os.path
 import struct
 import binascii
 
+import logging
+
 HEADER_BYTE_LENGTH = 16
 MAGIC_NUMBER = b'44334441' #0x41443344 # AD3D encoded as ASCII characters in hex
 VERSION = 1
 
-data3d_buffer_file = ''
+data3d_buffer_file = 'C:/Users/madlaina-kalunder/Desktop/test'
+logging.basicConfig(level='DEBUG', format='%(asctime)s %(levelname)-10s %(message)s')
+log = logging.getLogger('archilogic')
 
 
 def read_into_buffer(file):
@@ -18,10 +22,10 @@ def read_into_buffer(file):
 
 def get_header(buffer_file):
     header_array = [buffer_file[x:x+4] for x in range(0, HEADER_BYTE_LENGTH, 4)]
-    header = [  binascii.hexlify(header_array[0]),
-                binary_unpack('i', header_array[1]),
-                binary_unpack('i', header_array[2]),
-                binary_unpack('i', header_array[3]),
+    header = [binascii.hexlify(header_array[0]),
+              binary_unpack('i', header_array[1]),
+              binary_unpack('i', header_array[2]),
+              binary_unpack('i', header_array[3])
               ]
     print(header)
     return header
@@ -37,24 +41,28 @@ def get_header(buffer_file):
     #                 ]
     #     return header
 
+
 def binary_unpack(t, b):
     return struct.unpack(t, b)[0]
 
 
 def from_data3d_buffer(data3d_buffer):
-    file_buffer = read_into_buffer(data3d_buffer_file)
+    file_buffer = read_into_buffer(data3d_buffer)
+    print(file_buffer[0:4])
     magic_number, version, structure_byte_length, payload_byte_length = get_header(file_buffer)
-    expected_file_byte_length = HEADER_BYTE_LENGTH + structure_byte_length + payload_byte_length
+    expected_file_byte_length = HEADER_BYTE_LENGTH + structure_byte_length + payload_byte_length + 1
+
+    # Fixme why only != gives accurate result instead of is/is not
 
     # Validation warnings
     if magic_number != MAGIC_NUMBER:
-        print('File header error: Wrong magic number. File is probably not data3d buffer format.' + str(magic_number))
+        raise Exception('File header error: Wrong magic number. File is probably not data3d buffer format.' + str(magic_number))
     if version != VERSION:
-        print('File header error: Wrong version number: ' + str(version) + '. Parser supports version: ' + str(VERSION))
+        raise Exception('File header error: Wrong version number: ' + str(version) + '. Parser supports version: ' + str(VERSION))
 
     # Validation errors
-    print(len(file_buffer))
-    print(expected_file_byte_length)
+    if len(file_buffer) != expected_file_byte_length:
+        raise Exception('Can not parse data3d buffer. Wrong buffer size: ' + str(len(file_buffer)) + ' Expected: ' + str(expected_file_byte_length))
 
 
 from_data3d_buffer(data3d_buffer_file)
