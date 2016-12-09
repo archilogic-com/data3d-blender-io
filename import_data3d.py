91954b1915da1a8a30bce1951478a156e9ec58eb
Madlaina Kalunder <kalunder@archilogic.com>
2016-12-09 12:04:04 +0100

    Temporary disable tris to quads

--- import_data3d.py
+++ import_data3d.py
@@ -183,7 +183,7 @@ def import_scene(data3d_objects, **kwargs):
         O.object.mode_set(mode='EDIT')
         O.mesh.select_all(action='SELECT')
         O.mesh.remove_doubles(threshold=0.0001)
-        O.mesh.tris_convert_to_quads(face_threshold=0.174533, shape_threshold=3.14159, materials=True)
+        #O.mesh.tris_convert_to_quads(face_threshold=0.174533, shape_threshold=3.14159, materials=True)
         O.object.mode_set(mode='OBJECT')
 
     def create_mesh(data):
