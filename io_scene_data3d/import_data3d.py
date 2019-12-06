b62e3061d5f4cdea53e36452a114dfa62fab4027
Milan Redele <redele@archilogic.com>
2019-12-06 13:30:08 +0100

    Add prRev properties
    Properties needed by the viewer for reflections

--- io_scene_data3d/import_data3d.py
+++ io_scene_data3d/import_data3d.py
@@ -63,7 +63,9 @@ def import_data3d_materials(data3d_objects, filepath, import_metadata, place_hol
                         D3D.wf_thickness,
                         D3D.wf_color,
                         D3D.wf_opacity,
-                        D3D.bsdf_type
+                        D3D.bsdf_type,
+                        D3D.pr_rev_1,
+                        D3D.pr_rev_2
                         ]
         # Import material bake info for internal purposes.
         if import_metadata == 'ADVANCED':
